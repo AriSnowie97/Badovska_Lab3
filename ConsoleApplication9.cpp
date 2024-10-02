03dc13f912d54406804ae07e8a2abd9102e749e5
Ari Badovska <160342977+AriSnowie97@users.noreply.github.com>
2024-10-02 22:17:26 +0300

    Add files via upload

---
+++ ConsoleApplication9.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int number = 12345;
+    int reverse = 0;
+    do {
+        reverse = reverse * 10 + number % 10;
+        number /= 10;
+    } while (number);
+    cout << reverse << endl;
+    return 0;
+}
